bacc82d006dfd185d0ec0f23824484bd5505b31f
Carrotzpc <zhang.pc3@gmail.com>
2024-04-12 10:30:45 +0800

    ci(sdk): add extra param tree for hooks

--- .github/scripts/gen-bff-sdk.sh
+++ .github/scripts/gen-bff-sdk.sh
@@ -37,6 +37,8 @@ fi
 docker run --rm --net=host --env SDK_PACKAGE_NAME=@yuntijs/yunti-bff-sdk \
   --env SDK_YUNTI_NAME=YuntiBffClient --env GRAPH_API_ENDPOINT=${GRAPH_API_ENDPOINT} \
   --env GRAPH_CLIENT_ENDPOINT=${GRAPH_CLIENT_ENDPOINT} ${DOCKER_ENV_TEST_ONLY} \
+  --env HOOKS_EXTRA_PARAM=${HOOKS_EXTRA_PARAM} \
+  --env HOOKS_EXTRA_PARAM_DEFAULT_VALUE=${HOOKS_EXTRA_PARAM_DEFAULT_VALUE} \
   -v ${SERVER_PATH}/src:/schema \
   -v ~/.npmrc:/root/.npmrc ${GRL_SDK_GENERATOR_IMAGE}
 
